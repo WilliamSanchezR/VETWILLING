5f9fb989a8dc45785dd1f651fec2d626912b598d
William Sanchez <183452295+WilliamSanchezR@users.noreply.github.com~>
2025-12-02 17:29:11 -0500

    Pendiente de hacer cambios por el rol 4

--- app/controllers/loginControllers.php
+++ app/controllers/loginControllers.php
@@ -48,11 +48,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $rol = $resultado['id_rol'];
 
     switch ($rol) {
-        case 1: // Representante
+        case 1: // Administrador
             mostrarSweetAlert(
                 "success",
                 "Inicio de Sesión Exitoso",
-                "Bienvenido representante " . $resultado['nombres'],
+                "Bienvenido administrador " . $resultado['nombres'],
                 "/vetwilling/admin/dashBoard"
             );
             break;
