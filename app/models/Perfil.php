69790ddef3ca3aba4999950260c18c6da3df276b
William Sanchez <183452295+WilliamSanchezR@users.noreply.github.com~>
2025-12-03 16:24:40 -0500

    correcciones a la consulta de los roles

--- app/models/Perfil.php
+++ app/models/Perfil.php
@@ -44,16 +44,6 @@ class Perfil
                                 INNER JOIN administrador a ON u.id_usuario = a.id_usuario
                                 INNER JOIN rol r ON u.id_rol = r.id_rol
                                 WHERE u.id_usuario = :id";
-                case 1: // REPRESENTANTE
-                    $consultar = "SELECT 
-                                u.id_usuario, u.email, u.estado,
-                                r.nombres, r.apellidos, r.nivel_acceso, r.img_perfil, r.telefono,
-                                r.nombre AS rol
-                            FROM usuario u
-                            INNER JOIN representante_legal r ON u.id_usuario = r.id_usuario
-                            INNER JOIN rol r ON u.id_rol = r.id_rol
-                            WHERE u.id_usuario = :id";
-                    break;
 
                 case 2: // VETERINARIO
                     $consultar = "SELECT 
