d8acbcbfbd4c764ed0a4ca8eb6054b4349866d14
Adriana Castillo <adricasra1993@gmail.com>
2025-12-03 22:30:07 -0500

    Se agrega el menu para las opciones para la veterinaria

--- app/views/layouts/sidebar_administrador.php
+++ app/views/layouts/sidebar_administrador.php
@@ -18,12 +18,13 @@ $final_path = end($path_parts); // Obtiene el último elemento
     <div class="menu-sidebar">
         <div class="seccion-sidebar">Menu Administrador</div>
 
-        <a href="<?= BASE_URL ?>/admin/dashBoard" class="item-sidebar <?= $final_path == 'dashBoard' ? 'active': '' ?>">
+        <a href="<?= BASE_URL ?>/admin/dashBoard" class="item-sidebar <?= $final_path == 'dashBoard' ? 'active' : '' ?>">
             <i class="bi bi-speedometer2"></i>
             <span class="texto-item-sidebar">Dashboard</span>
         </a>
-
-        <div class="item-sidebar submenu <?= $final_path == 'registro-usuario' || $final_path == 'listar-usuarios' ? 'active': '' ?>">
+        
+        <!-- Menu Usuario -->
+        <div class="item-sidebar submenu <?= $final_path == 'registro-usuario' || $final_path == 'listar-usuarios' ? 'active' : '' ?>">
 
             <a href="#" class="submenu-toggle">
                 <i class="bi bi-person-plus"></i>
@@ -32,10 +33,27 @@ $final_path = end($path_parts); // Obtiene el último elemento
             </a>
 
             <ul class="submenu-items">
-                <li><a class="<?= $final_path == 'registro-usuario' ? 'active': '' ?>" href="<?= BASE_URL ?>/admin/registro-usuario">Registrar </a></li>
-                <li><a class="<?= $final_path == 'listar-usuarios' ? 'active': '' ?>" href="<?= BASE_URL ?>/admin/listar-usuarios">Listar</a></li>
+                <li><a class="<?= $final_path == 'registro-usuario' ? 'active' : '' ?>" href="<?= BASE_URL ?>/admin/registro-usuario">Registrar </a></li>
+                <li><a class="<?= $final_path == 'listar-usuarios' ? 'active' : '' ?>" href="<?= BASE_URL ?>/admin/listar-usuarios">Listar</a></li>
             </ul>
         </div>
+        
+        <!-- Menu Veterinaria -->
+        <div class="item-sidebar submenu <?= $final_path == 'registro-veterinaria' || $final_path == 'listar-veterinarias' ? 'active' : '' ?>">
+
+            <a href="#" class="submenu-toggle">
+                <i class="bi bi-person-plus"></i>
+                <span class="texto-item-sidebar">Veterinaria</span>
+                <i class="bi bi-chevron-down flecha"></i>
+            </a>
+
+            <ul class="submenu-items">
+                <li><a class="<?= $final_path == 'registro-veterinaria' ? 'active' : '' ?>" href="<?= BASE_URL ?>/admin/registro-veterinaria">Registrar </a></li>
+                <li><a class="<?= $final_path == 'listar-veterinarias' ? 'active' : '' ?>" href="<?= BASE_URL ?>/admin/listar-veterinarias">Listar</a></li>
+            </ul>
+        </div>
+
+
 
         <a href="<?= BASE_URL ?>/logout" class="item-sidebar">
             <i class="bi bi-box-arrow-in-left"></i>
