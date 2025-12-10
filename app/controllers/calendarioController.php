6f6f49f57c790025acb5fea593b829d4635e2ed7
William Sanchez <sanchezwill596@gmail.com>
2025-12-10 15:55:33 -0500

    Creacion de los archivos necesarios para el manejo del calnedario para la asignacion de eventos en el mismo, pendiente de agregar la conexion con la base de datos para su correcto funcionamiento

---
+++ app/controllers/calendarioController.php
@@ -0,0 +1,12 @@
+<?php
+
+// Importamos las dependencias
+
+require __DIR__ . '/../helpers/alert_helpers.php';
+require __DIR__ . '/../models/Eventos.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+}
+
+// Capturamos en variables los valores enviados a traves de los names de formulario y el metho POST 
+
